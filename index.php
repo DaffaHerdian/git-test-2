3ed150a73d6c99c2deeee631e4168fdb4bfeb497
Daffa Herdian <daffaahmad0517@gmail.com>
2025-06-11 03:43:41 +0700

    menambahkan list di index php

--- index.php
+++ index.php
@@ -10,6 +10,12 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure esse debitis quis tenetur nemo, nesciunt enim placeat natus vero nulla, temporibus quisquam, quod voluptatum. Sit nihil corporis aut totam laudantium?</p>
+
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
+
 </body>
 
 </html>
